6a0a832846f51baa96a2a74df35681f9d7104630
Shunsuke Kimura <kimushun1101@gmail.com>
2024-12-22 08:30:45 +0900

    Refactor publish_collision_poly node
    Signed-off-by: Shunsuke Kimura <kimushun1101@gmail.com>

--- script/rec_bag.sh
+++ script/rec_bag.sh
@@ -4,7 +4,7 @@ mkdir -p ${output_directory}
 current_time=$(date '+%Y-%m-%d-%H-%M-%S')
 ros2 bag record -o ${output_directory}/turtlebot3_${current_time} \
   /cbf_debug \
-  cbf_debug_collision \
+  /cbf_debug_collision \
   /cmd_vel \
   /cmd_vel_ref \
   /odom \
@@ -12,18 +12,3 @@ ros2 bag record -o ${output_directory}/turtlebot3_${current_time} \
   /scan \
   /tf \
   /tf_static
-
-  # The script above records the following topics: 
-  
-  # /cbf_debug 
-  # /cmd_vel_ref 
-  # /odom 
-  # /collision_polygon 
-  # /scan 
-  # /cmd_vel 
-  
-  # The recorded bag file is saved in the same directory as the script. 
-  # The following is the command to run the script: 
-  # $ bash rec_bag.sh
-  
-  # The recorded bag file is saved in the same directory as the script.
--- src/publish_collision_polygon.cpp
+++ src/publish_collision_polygon.cpp
@@ -19,19 +19,16 @@ PublishCollisionPolygon::PublishCollisionPolygon() : Node("publish_collision_pol
 {
   this->declare_parameter("base_frame_id", "base_link");
   base_frame_id_ = this->get_parameter("base_frame_id").as_string();
-
   this->declare_parameter("plot_count", 50);
   plot_count_ = this->get_parameter("plot_count").as_int();
+  this->declare_parameter("circle.x", -0.032);
+  circle_.x = this->get_parameter("circle.x").as_double();
+  this->declare_parameter("circle.y", 0.0);
+  circle_.y = this->get_parameter("circle.y").as_double();
+  this->declare_parameter("circle.r", 0.35);
+  circle_.r = this->get_parameter("circle.r").as_double();
 
-  this->declare_parameter("robot.x", 0.06);
-  this->declare_parameter("robot.y", 0.0);
-  this->declare_parameter("robot.r", sqrt(0.16*0.16 + 0.085*0.085));
-  robot_.x = this->get_parameter("robot.x").as_double();
-  robot_.y = this->get_parameter("robot.y").as_double();
-  robot_.r = this->get_parameter("robot.r").as_double();
-
-
-  robot_poly_pub_ = this->create_publisher<geometry_msgs::msg::PolygonStamped>("robot_polygon", 10);
+  collision_poly_pub_ = this->create_publisher<geometry_msgs::msg::PolygonStamped>("collision_polygon", 10);
   std::chrono::milliseconds sampling_period{(int)(1000.0)};
   timer_ = this->create_wall_timer(
     sampling_period, std::bind(&PublishCollisionPolygon::timer_callback, this));
@@ -46,27 +43,17 @@ PublishCollisionPolygon::~PublishCollisionPolygon()
 
 void PublishCollisionPolygon::timer_callback()
 {
-  publish_circle(robot_, 30, robot_poly_pub_);
-}
-
-void PublishCollisionPolygon::publish_circle(const Circle& c, std::size_t plot_count, const rclcpp::Publisher<geometry_msgs::msg::PolygonStamped>::SharedPtr pub)
-{
   auto poly = geometry_msgs::msg::PolygonStamped();
   poly.header.stamp = this->now();
   poly.header.frame_id = base_frame_id_;
   auto point = geometry_msgs::msg::Point32();
-  double delta = 2*M_PI/(double)plot_count;
+  double delta = 2 * M_PI / static_cast<double>(plot_count_);
   double theta = 0.0;
-
-  double r, offset_x, offset_y;
-  offset_x = c.x;
-  offset_y = c.y;
-  r = c.r;
-  for(std::size_t i = 0; i < plot_count; i++){
-    point.x = r*cos(theta) + offset_x;
-    point.y = r*sin(theta) + offset_y;
+  for (std::size_t i = 0; i < plot_count_; i++) {
+    point.x = circle_.r * cos(theta) + circle_.x;
+    point.y = circle_.r * sin(theta) + circle_.y;
     poly.polygon.points.push_back(point);
     theta += delta;
   }
-  pub->publish(poly);
+  collision_poly_pub_->publish(poly);
 }
